a562cddca8af01f2a399e2b621ccd7b75f5de493
Kate Baldwin <marykatbaldwin@gmail.com>
2023-11-04 15:45:10 -0500

    contacted the contacs

--- Dynamic Site/Controllers/HomeController.cs
+++ Dynamic Site/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using Dynamic_Site.Models;
+﻿using Dynamic_Site.Models; //Grants access to ContactViewModel
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -56,11 +56,104 @@ namespace Dynamic_Site.Controllers
 			return View();
 		}
 
+          public IActionResult Contact(ContactViewModel cvm)
+          {
+               
+               if (!ModelState.IsValid)
+               {
+                    
+
+                    return View(cvm);
+               }
+
+               
+
+               #region Email Setup Steps & Email Info
+
+               //1. Go to Tools > NuGet Package Manager > Manage NuGet Packages for Solution
+               //2. Go to the Browse tab and search for NETCore.MailKit
+               //3. Click NETCore.MailKit
+               //4. On the right, check the box next to the CORE1 project, then click "Install"
+               //5. Once installed, return here
+               //6. Add the following using statements & comments:
+               //      - using MimeKit; //Added for access to MimeMessage class
+               //      - using MailKit.Net.Smtp; //Added for access to SmtpClient class
+               //7. Once added, return here to continue coding email functionality
+
+               //MIME - Multipurpose Internet Mail Extensions - Allows email to contain
+               //information other than ASCII, including audio, video, images, and HTML
+
+               //SMTP - Simple Mail Transfer Protocol - An internet protocol (similar to HTTP)
+               //that specializes in the collection & transfer of email data
+
+               #endregion
+
+               //format
+               string message = $"You have received a new email from your site's contact form!<br />" +
+                   $"Sender: {cvm.Name}<br/>Email: {cvm.Email}<br />Subject: {cvm.Subject}<br />Message: {cvm.Message}";
+
+               // MimeMessage 
+               var mm = new MimeMessage();
+
+            
+
+               // credentials for this email user 
+               mm.From.Add(new MailboxAddress("Sender", _config.GetValue<string>("Credentials:Email:User")));
+
+               // our personal email address
+               mm.To.Add(new MailboxAddress("Personal", _config.GetValue<string>("Credentials:Email:Recipient")));
+
+               mm.Subject = cvm.Subject;
+
+               //The body of the email will be formatted with the string we created above
+               mm.Body = new TextPart("HTML") { Text = message };
+
+               //"urgent" so it will be flagged in our email client
+               mm.Priority = MessagePriority.Urgent;
+
+               //user's email address to the list of ReplyTo addresses.
+               mm.ReplyTo.Add(new MailboxAddress("User", cvm.Email));
+
+               // create the SmtpClient object used to send the email. 
+               using (var client = new SmtpClient())
+               {
+                    //Connect to the mail server
+                    client.Connect(_config.GetValue<string>("Credentials:Email:Client"), 8889);
+
+                    //Log in to the mail server using the credentials for our email user.
+                    client.Authenticate(
+
+                        //Username
+                        _config.GetValue<string>("Credentials:Email:User"),
+
+                        //Password
+                        _config.GetValue<string>("Credentials:Email:Password")
+
+                        );
+
+                    
+                    try
+                    {
+                         client.Send(mm);
+                    }
+                    catch (Exception ex)
+                    {
+                         //If there's an issue, we can store an error message in a ViewBag variable 
+
+                         ViewBag.ErrorMessage = $"There was an error processing your request. Please try again later.<br />" +
+                             $"Error Message: {ex.StackTrace}";
+
+                         return View(cvm);
+                    }
+
+               }
+               return View("EmailConfirmation", cvm);
+          }
 
 
 
 
 
 
-	}
+     }
 }
--- Dynamic Site/Models/ContactViewModel.cs
+++ Dynamic Site/Models/ContactViewModel.cs
@@ -7,23 +7,22 @@ namespace Dynamic_Site.Models
 	public class ContactViewModel
 	{
 
-		//We can use Data Annotation to add validation to our model
-		//This is useful when we have required fields or need certain kinds of information
+	
 
-
-		[Required(ErrorMessage = "*Name is required")]//Makes the field required
-		public string? Name { get; set; }
 
 		[Required(ErrorMessage = "*Name is required")]
-		[DataType(DataType.EmailAddress)]//Certain formatting is expected
-		public string? Email { get; set; }
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "*Name is required")]
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
 
 		[Required(ErrorMessage = "*Subject is required")]
-		public string? Subject { get; set; }
+		public string Subject { get; set; }
 
 		[Required(ErrorMessage = "*Message is required")]
-		[DataType(DataType.MultilineText)] //Denotes this field is larger than a standard textbox (<input> => <textarea>)
-		public string? Message { get; set; }
+		[DataType(DataType.MultilineText)] 
+		public string Message { get; set; }
 
 
 
